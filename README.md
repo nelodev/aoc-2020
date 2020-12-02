09fc83edcb2212750519732ef5245d76c9765a26
Nelo Puchades <correo@nelo.dev>
2020-12-02 18:25:55 +0100

    First and second days of Advent of Code 2020

---
+++ README.md
@@ -0,0 +1,6 @@
+# Adventa of Code 2020 🎅🏻
+
+## Day 1 - 1/12/20 - Report Repair ✅ ⭐️ ⭐️
+## Day 2 - 2/12/20 - Password Philosophy ✅ ⭐️ ⭐️
+
+- Base.js utility is an idea of @rubnvp
---
+++ base.js
@@ -0,0 +1,12 @@
+const inputText = ``;
+
+function parseInput(inputText) {
+  return inputText;
+}
+
+function resolve1(input) {
+  return input;
+}
+
+const input = parseInput(inputText);
+const output = resolve1(input);
